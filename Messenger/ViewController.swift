e1a7e57dceba99bfba55c189cd6026dd1b619f96
dawidplesniarski <dawidplesniarski@gmail.com>
2020-01-08 21:51:16 +0100

    Message Sendung works.

--- Messenger/ViewController.swift
+++ Messenger/ViewController.swift
@@ -18,14 +18,69 @@ struct User: Codable{
 class ViewController: UIViewController{
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textView: UITextView!
     
     //var messagesArray:[(content: String, login: String, date: String)] = []
     var messagesArray:[String] = []
+    let myLogin = "Dawid"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
         print(messagesArray.count)
+    }
+    
+    @IBAction func sendBtnPressed(_ sender: Any) {
+       sendMsg()
+    }
+    
+    func sendMsg(){
+        let parameters = ["content": textView.text ?? "message", "login": myLogin] as [String : Any]
+
+            //create the url with URL
+            let url = URL(string: "http://tgryl.pl/shoutbox/message")!
+
+            //create the session object
+            let session = URLSession.shared
+
+            //now create the URLRequest object using the url object
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST" //set http method as POST
+
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            } catch let error {
+                print(error.localizedDescription)
+            }
+
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+            //create dataTask using the session object to send data to the server
+            let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+
+                guard error == nil else {
+                    return
+                }
+
+                guard let data = data else {
+                    return
+                }
+
+                do {
+                    //create json object from data
+                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        print(json)
+                        // handle json...
+                    }
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            })task.resume()
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+
     }
     
     
@@ -45,7 +100,7 @@ class ViewController: UIViewController{
                     print("Login: " + message.login)
                     print("Date: " + message.date)
                     //self.messagesArray.append((message.content,message.login,message.date))
-                    self.messagesArray.append("Content: "+message.content+"Login: "+message.login+"Date "+message.date)
+                    self.messagesArray.append("Content: "+message.content+"\nLogin: "+message.login+"\nDate "+message.date)
                 }
             }catch let jsonErr{
                 print(jsonErr)
@@ -56,7 +111,6 @@ class ViewController: UIViewController{
             }
 
         }.resume()
-
     }
 }
 
