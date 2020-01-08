ea60d056d2eb1570c2fdc9b49201ff72fa76677b
dawidplesniarski <dawidplesniarski@gmail.com>
2020-01-08 15:42:05 +0100

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Dawid on 08/01/2020.
+//  Copyright © 2020 Dawid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
